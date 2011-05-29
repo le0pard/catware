7865be919a2fae21c813a5f4fcb21bbb324602b6
Alexey <leopard.not.a@gmail.com>
2011-05-29 20:09:28 +0300

    fix i18n

--- app/controllers/admin/user_sessions_controller.rb
+++ app/controllers/admin/user_sessions_controller.rb
@@ -9,17 +9,17 @@ class Admin::UserSessionsController < ApplicationController
   def create
     if user = User.find_by_email(params[:user][:email]).try(:authenticate, params[:user][:password])
       session[:user_id] = user.id
-      flash[:notice] = t('user_sessions.create.flash.notice')
+      flash[:notice] = t('admin.user_sessions.create.flash.notice')
       redirect_to admin_root_path
     else
-      flash[:error] = t('user_sessions.create.flash.error')
+      flash[:error] = t('admin.user_sessions.create.flash.error')
       redirect_to :action => :new
     end
   end
  
   def destroy
     session.delete(:user_id)
-    flash[:notice] = t('user_sessions.destroy.flash.notice')
+    flash[:notice] = t('admin.user_sessions.destroy.flash.notice')
     redirect_to :action => :new
   end
 end
