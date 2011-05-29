52f14cf271e172c435e7492079e0c06bbde6bf50
Alexey <leopard.not.a@gmail.com>
2011-05-29 16:28:41 +0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Catware::Application.config.secret_token = 'e4f88f89f805470e22900dacac809cf5cc4401093df0fda9a9af7ae9ff6c708b9b1cb5271a5c8ba0ffefe5dea91c201d7e24d550a1c18358d635595fffc5b046'
