fb6500114bcdf9a467eac6627380b45f0c119f4d
Alexey <leopard.not.a@gmail.com>
2011-06-01 00:19:45 +0300

    Finished users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  
+  def project_menu_item(title, link, with_action = false, add_class = "")
+    route_hash = Rails.application.routes.recognize_path(link) rescue {:controller => nil, :action => nil}
+    unless route_hash[:controller].nil?
+      boolean_menu = false
+      if with_action
+        boolean_menu = (controller.controller_path == route_hash[:controller] && controller.action_name == route_hash[:action])
+      else
+        boolean_menu = (controller.controller_path == route_hash[:controller])
+      end
+      active_li = "class=\"#{add_class}\""
+      active_li = "class=\"active #{add_class}\"" if boolean_menu
+      "<li #{active_li}>#{link_to(title, link)}</li>".html_safe
+    end
+  end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   
   validates :email, :presence => true, :uniqueness => true, :length => { :maximum => 100 }
   validates :password, :presence => { :on => :create }, :length => { :maximum => 100 }
+  
+  
 end
