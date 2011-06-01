be92f5efbe617eacb248be9d23d3d3cd875c8caa
Alexey <leopard.not.a@gmail.com>
2011-06-02 02:04:15 +0300

    deploy

---
+++ config/deploy.rb
@@ -0,0 +1,24 @@
+require "capistrano/ext/multistage"
+require "bundler/capistrano"
+Dir['lib/capistrano_recipes/tasks/*.rb'].each { |task| require task }
+
+set :stages, %w(production)
+set :default_stage, "production"
+
+set :scm, :git
+set :scm_verbose, false
+set :repository,  "git://github.com/le0pard/catware.git"
+
+set :deploy_via, :remote_cache
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
+
+set :keep_releases, 3
+
+namespace :deploy do
+   task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+end
---
+++ config/deploy/production.rb
@@ -0,0 +1,26 @@
+set :application, "catware"
+set :rails_env, "production"
+
+ssh_options[:user] = "deployer"
+ssh_options[:keys] = ["~/.ssh/id_dsa"]
+ssh_options[:port] = 9090
+
+set :main_server, "92.249.81.1"
+
+role :app, main_server
+role :web, main_server
+role :db,  main_server, :primary => true
+
+set :user, 'deployer'
+set :group, 'deployer'
+set :private_db_ip, "localhost"
+
+set :branch, :master
+
+set :app_server, :passanger
+
+set :use_sudo, false
+
+set :deploy_to, "/var/www/apps/#{application}"
+
+after "deploy", "deploy:migrate", "deploy:cleanup"
