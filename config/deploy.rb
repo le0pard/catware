03e93bef57fd67d782a70eb416a077fc2a180230
Alexey <leopard.not.a@gmail.com>
2011-06-02 10:48:44 +0300

    cap require

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,7 @@
 require "capistrano/ext/multistage"
 require "bundler/capistrano"
-Dir['lib/capistrano_recipes/tasks/*.rb'].each { |task| require task }
+require File.expand_path('../../lib/capistrano_recipes/tasks/capistrano_database_yml.rb', __FILE__)
+#Dir['lib/capistrano_recipes/tasks/*.rb'].each { |task| require task }
 
 set :stages, %w(production)
 set :default_stage, "production"
@@ -15,10 +16,10 @@ default_run_options[:pty] = true
 
 set :keep_releases, 3
 
-namespace :deploy do
-   task :start do ; end
-   task :stop do ; end
-   task :restart, :roles => :app, :except => { :no_release => true } do
-     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-   end
-end
+#namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+#end
