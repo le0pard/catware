a8d0df9d6d3dae30b2a77f28f0b1433fdf7ca8a3
Alexey <leopard.not.a@gmail.com>
2011-06-04 18:03:37 +0300

    Project admin

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -1,7 +1,44 @@
 class Admin::ProjectsController < Admin::BaseController
+  before_filter :find_project, :only => [:edit, :update, :destroy]
   
   def index
     @projects = Project.order("created_at DESC").page(params[:page])
+  end
+  
+  def new
+    @project = Project.new
+  end
+  
+  def create
+    @project = Project.new(params[:project])
+    if @project.save
+      redirect_to :action => :edit, :id => @project.id
+    else
+      render :action => :new
+    end
+  end
+  
+  def edit
+    #
+  end
+  
+  def update
+    if @project.update_attributes(params[:project])
+      redirect_to :action => :edit, :id => @project.id
+    else
+      render :action => :edit, :id => @project.id
+    end
+  end
+  
+  def destroy
+    @project.destroy
+    redirect_to :action => :index
+  end
+  
+  private
+  
+  def find_project
+    @project = Project.find(params[:id])
   end
   
 end
