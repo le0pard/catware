3bc88cba997e138551c348575122cb8c1a5d2950
Alexey <leopard.not.a@gmail.com>
2011-06-04 23:38:24 +0300

    contacts

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,11 +7,15 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(params[:contact])
     if @contact.valid?
-      flash[:notice] = "Thank you"
-      redirect_to :action => :index
+      #flash[:notice] = "Thank you"
+      redirect_to :action => :thank_you
     else
       render :action => :index
     end
+  end
+  
+  def thank_you
+    
   end
   
 end
