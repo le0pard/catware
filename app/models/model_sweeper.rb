d2b816b3c5fcb03716f11b1e2bbcb83b2368b449
Alexey <leopard.not.a@gmail.com>
2011-06-28 19:40:20 +0300

    cache

--- app/models/model_sweeper.rb
+++ app/models/model_sweeper.rb
@@ -15,15 +15,15 @@ class ModelSweeper < ActionController::Caching::Sweeper
  
   private
   def expire_cache_for(object)
-    expire_page(:controller => 'projects', :action => 'index')
+    expire_page(:controller => '/projects', :action => 'index')
     Project.all.each do |project|
-      expire_page(:controller => 'projects', :action => 'show', :id => project)
+      expire_page(:controller => '/projects', :action => 'show', :id => project)
     end
-    expire_page(:controller => 'services', :action => 'mobile')
-    expire_page(:controller => 'services', :action => 'web')
-    expire_page(:controller => 'services', :action => 'mobile_news')
-    expire_page(:controller => 'contacts', :action => 'thank_you')
-    expire_page(:controller => 'errors', :action => 'page_not_found')
-    expire_page(:controller => 'dashboard', :action => 'index')
+    expire_page(:controller => '/services', :action => 'mobile')
+    expire_page(:controller => '/services', :action => 'web')
+    expire_page(:controller => '/services', :action => 'mobile_news')
+    expire_page(:controller => '/contacts', :action => 'thank_you')
+    expire_page(:controller => '/errors', :action => 'page_not_found')
+    expire_page(:controller => '/dashboard', :action => 'index')
   end
 end
