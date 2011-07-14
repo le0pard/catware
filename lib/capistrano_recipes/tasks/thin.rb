ba4b5f9ba98feb7dd2d3d06ada448b28680b35c0
Alexey <leopard.not.a@gmail.com>
2011-07-14 21:41:06 +0300

    fix

--- lib/capistrano_recipes/tasks/thin.rb
+++ lib/capistrano_recipes/tasks/thin.rb
@@ -14,7 +14,7 @@ Capistrano::Configuration.instance.load do
       task :setup, :except => { :no_release => true } do
         run "mkdir -p #{shared_path}/db" 
         run "mkdir -p #{shared_path}/config" 
-        run "cd #{current_path}; bundle exec thin config -C #{shared_path}/config/thin.yml -c #{current_path} -e #{fetch(:rails_env, "production")} -d -s 6 -u #{fetch(:user, "deployer")} -g #{fetch(:group, "deployer")} -l #{shared_path}/log/thin.log -P #{shared_path}/tmp/pids/thin.pid -S #{shared_path}/tmp/sockets/thin.sock"
+        run "cd #{current_path}; bundle exec thin config -C #{shared_path}/config/thin.yml -c #{current_path} -e #{fetch(:rails_env, "production")} -d -s 2 -u #{fetch(:user, "deployer")} -g #{fetch(:group, "deployer")} -l #{shared_path}/log/thin.log -P #{shared_path}/tmp/pids/thin.pid -S #{shared_path}/tmp/sockets/thin.sock"
       end
       
       desc <<-DESC
