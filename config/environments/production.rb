1bef4d3d2fa8f3554c636dc3f96bdde966c55349
Alexey <leopard.not.a@gmail.com>
2011-09-05 08:30:12 +0300

    fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,19 +7,16 @@ Catware::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  
-  config.action_controller.page_cache_directory = "#{Rails.public_path}/cache"
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
 
-  # Compress both stylesheets and JavaScripts
-  config.assets.js_compressor  = :uglifier
-  config.assets.css_compressor = :scss
+  # Compress JavaScripts and CSS
+  config.assets.compress = true
 
   # Specifies the header that your server uses for sending files
-  # (comment out if your front-end server doesn't support this)
-  config.action_dispatch.x_sendfile_header = "X-Sendfile" # Use 'X-Accel-Redirect' for nginx
+  # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
+  # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
